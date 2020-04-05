5ce86c3edd7db3fe11b5165a4abdcb0450f269a3
David Turnbull <dturnbull@gmail.com>
2020-04-05 10:37:53 -0700

    write the book

--- src/doc/introduction.rs
+++ src/doc/introduction.rs
@@ -1,15 +1,16 @@
 /*!
-# Introduction to BASIC
+# Introductory Tutorial for 64K BASIC
 
 Begin by opening a terminal and running the executable. Double clicking
 the executable from a GUI desktop often works as well. If you get the
-following, you have achieved success and are ready for this short tutorial.
-To exit 64K BASIC type CTRL-D.
-```text
-64K BASIC
-READY.
-█
-```
+following, you have achieved success and are ready for this tutorial.
+Type CTRL-D top exit 64K BASIC.
+<pre><code>&nbsp;  64K BASIC
+&nbsp;  READY.
+&nbsp;> █
+</code></pre>
+
+ Stop a running program with CTRL-C.
 
 64K BASIC is interactive just like it was back in 1964 when the idea of an
 ordinary person sitting in front of a terminal and directly interacting
@@ -70,10 +71,9 @@ first try. You can access a history of direct mode statements with the up/down a
 &nbsp;> PAINT "Hello World"
 </code></pre>
 
-Let's jump ahead a bit to saving and loading. You can `SAVE` a program
-to the filesystem or `LOAD` one that you previously saved or downloaded.
-Filenames are specified absolute or relative to the current directory of
-your operating system when 64K BASIC was started.
+You can `SAVE` a program to the filesystem or `LOAD` one that you previously
+saved or downloaded. Filenames are specified absolute or relative to the
+current directory of your operating system when 64K BASIC was started.
 The `NEW` command erases the program in memory.
 
 <pre><code>&nbsp;> 10 print "Hello World
@@ -88,5 +88,41 @@ The `NEW` command erases the program in memory.
 &nbsp;> list
 &nbsp;> 10 PRINT "Hello World"
 </code></pre>
+
+Let's create a multi-line program for the last example of this tutorial.
+The program will ask the user for a number, print its square root, and repeat
+indefinitely. Because this is an infinite loop, the program will run forever
+or until it's interrupted. Typing CTRL-C interrupts a program.
+
+<pre><code>&nbsp;> 10 input "Your number"; a
+&nbsp;> 20 print "The square root of" a "is" sqr(a)
+&nbsp;> 30 goto 100
+&nbsp;> run
+&nbsp;  <b>?UNDEFINED LINE IN 30:9</b>
+&nbsp;> list 30
+&nbsp;> 30 GOTO <u>100</u>
+&nbsp;  READY.
+&nbsp;> 30 GOTO 10 <i>{Remember to use TAB here}</i>
+&nbsp;> run
+&nbsp;> Your number? -8
+&nbsp;  The square root of-8 is NaN
+&nbsp;> Your number? 9
+&nbsp;  The square root of 9 is 3
+&nbsp;> Your number?<i>{CTRL-C}</i>
+&nbsp;  <b>?BREAK IN 10</b>
+&nbsp;  READY.
+</code></pre>
+
+Line 30 was intentionally wrong to demonstrate a compiler error. 64K BASIC
+has two types of errors, compiler errors and runtime errors. A lot more
+information is available at compile time which enables underlining the
+offending section when listing a program. Runtime errors will only display
+the line number.
+
+This concludes the introductory tutorial. The remainder of this manual is
+reference material covering everything 64K BASIC can do. There's a lot of
+programs which will run without modification, but every implementation of
+BASIC has quirks. Appendix A has information about these quirks and
+suggestions for converting them to the 64K BASIC dialect.
 
 */
