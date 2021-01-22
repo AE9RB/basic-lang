c9b2e4c14bf238a35f216544051caa03364b5912
David Turnbull <dturnbull@gmail.com>
2021-01-22 15:36:03 -0800

    improve lang module docs

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -3,6 +3,18 @@
 
 This Rust module provides lexical analysis and parsing of the BASIC language.
 
+64K BASIC aims to be compatible with the BASIC that was popular among
+8-bit computers in the 1970s and 80s. These BASIC interpreters would typically
+occupy 4 to 10 kilobytes of an available 64k memory space. Every byte of RAM was
+precious. BASIC programs were kept in memory using a trivial compression method
+where known words would only occupy a single byte. This infallible tokenization
+was the only thing done before a program was run. The interpreter used the token
+stream as a crude VM instruction set.
+
+64K BASIC performs context-sensitive lexical analysis and parses to an
+abstract syntax tree (AST). The lex() and parse() functions are exposed but
+it is recommended to use tokens() and ast() from Line instead.
+
 */
 
 pub type Column = std::ops::Range<usize>;
